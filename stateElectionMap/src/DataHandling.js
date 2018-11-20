2cc9362cdd483e91a7f19d82a1684673eaf2d865
Greg Stoll <greg@gregstoll.com>
2018-11-19 22:12:05 -0600

    handle multiple years of data, kinda

--- stateElectionMap/src/DataHandling.js
+++ stateElectionMap/src/DataHandling.js
@@ -7,21 +7,31 @@ const cleanUSStateName = (d) => ({
     name: d.name
 });
 
-//TODO
-const cleanElectionResults = d => ({
-    stateCode: d[""],
-    dCount: Number(d["Obama (D)"].replace(/,/g, '')),
-    rCount: Number(d["Romney (R)"].replace(/,/g, ''))
-});
+const cleanElectionResults = d => {
+    let dKey = _.find(Object.keys(d), key => key.endsWith("(D)"));
+    let rKey = _.find(Object.keys(d), key => key.endsWith("(R)"));
+    return {
+        stateCode: d[""],
+        dCount: Number(d[dKey].replace(/,/g, '')),
+        rCount: Number(d[rKey].replace(/,/g, ''))
+    };
+};
 
-const electionYears = [2012];
+const electionYears = [2008, 2012];
 
 //TODO - probably can make the caller async-aware
 export const loadAllData = async (callback = _.noop) => {
     //TODO - parallelize
     let us = await d3.json('data/us.json');
     let usStateNames = await d3.tsv('data/us-state-names.tsv', cleanUSStateName);
-    let electionData = await d3.csv('data/electionResults/2012.csv', cleanElectionResults);
+    let electionDataPromises = {};
+    electionYears.forEach(value => {
+        electionDataPromises[value] = d3.csv('data/electionResults/' + value + '.csv', cleanElectionResults);
+    });
+    let electionData = {}; //await d3.csv('data/electionResults/2012.csv', cleanElectionResults);
+    for (let i = 0; i < electionYears.length; ++i) {
+        electionData[electionYears[i]] = await electionDataPromises[electionYears[i]];
+    }
 	callback({
 	    usTopoJson: us,
         usStateNames: usStateNames,
