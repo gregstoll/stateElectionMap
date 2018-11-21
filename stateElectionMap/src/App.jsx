8425de54bf6b5c69b37c902b5b66fc3655f29d4c
Greg Stoll <greg@gregstoll.com>
2018-11-21 16:04:19 -0600

    show shades of colors based on margin

--- stateElectionMap/src/App.jsx
+++ stateElectionMap/src/App.jsx
@@ -18,6 +18,45 @@ class App extends Component {
         });
     }
 
+    colorFromDAndRVote(dVote, rVote) {
+        // http://colorbrewer2.org/?type=diverging&scheme=RdBu&n=11
+        const _colors =
+            ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#f7f7f7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac', '#053061'];
+        
+        let dPercentage = (dVote * 100.0) / (dVote + rVote);
+        // 5 red, 5 blue (don't use middle one)
+        const increment = 3;
+        if (dPercentage < 50 - 4 * increment) {
+            return _colors[0];
+        }
+        if (dPercentage < 50 - 3 * increment) {
+            return _colors[1];
+        }
+        if (dPercentage < 50 - 2 * increment) {
+            return _colors[2];
+        }
+        if (dPercentage < 50 - 1 * increment) {
+            return _colors[3];
+        }
+        if (dPercentage < 50) {
+            return _colors[4];
+        }
+
+        if (dPercentage > 50 + 4 * increment) {
+            return _colors[10];
+        }
+        if (dPercentage > 50 + 3 * increment) {
+            return _colors[9];
+        }
+        if (dPercentage > 50 + 2 * increment) {
+            return _colors[8];
+        }
+        if (dPercentage > 50 + increment) {
+            return _colors[7];
+        }
+        return _colors[6];
+    }
+
     onSliderChange = (value) => {
         this.setState({ year: value });
     }
@@ -35,7 +74,7 @@ class App extends Component {
             let stateData = _.find(this.state.electionData[this.state.year], electionDataObj => electionDataObj.stateCode === stateCode);
             if (stateData) {
                 // TODO - cooler stuff
-                stateColors[stateCode] = stateData.rCount > stateData.dCount ? '#ff0000' : '#0000ff';
+                stateColors[stateCode] = this.colorFromDAndRVote(stateData.dCount, stateData.rCount);
             }
         }
     }
