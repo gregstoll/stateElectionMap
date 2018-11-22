7a2dfe9333ff65b0926a89529915d23ed34465c6
Greg Stoll <greg@gregstoll.com>
2018-11-21 22:25:17 -0600

    Add a button to show results versus national popular vote, fix TypeScript debugging

--- stateElectionMap/src/App.tsx
+++ stateElectionMap/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StateMap } from './StateMap';
+import { Button } from 'semantic-ui-react';
 import _ from 'lodash';
 import { loadAllData, DataCollection, StateName, ElectionData, MIN_YEAR, MAX_YEAR, YEAR_STEP } from './DataHandling';
 import Slider, { createSliderWithTooltip} from 'rc-slider';
@@ -9,6 +10,7 @@ import './App.css';
 
 interface AppState {
     year: number,
+    rawResults: boolean,
     //TODO - figure out this type and use it everywhere
     usTopoJson: any,
     stateNames: StateName[],
@@ -16,8 +18,9 @@ interface AppState {
 }
 
 class App extends Component<{}, AppState> {
-    state = {
+    state : AppState = {
         year: 0,
+        rawResults: true,
         usTopoJson: null,
         stateNames: null,
         electionData: null
@@ -33,7 +36,7 @@ class App extends Component<{}, AppState> {
         this.setState(Object.assign(yearState, data));
     }
 
-    colorFromDAndRVote(dVote: number, rVote: number, baseline = 50) {
+    colorFromDAndRVote(dVote: number, rVote: number, baselineDPercentage = 50) {
         // http://colorbrewer2.org/?type=diverging&scheme=RdBu&n=11
         const _colors =
             ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#f7f7f7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac', '#053061'];
@@ -41,35 +44,45 @@ class App extends Component<{}, AppState> {
         let dPercentage = (dVote * 100.0) / (dVote + rVote);
         // 5 red, 5 blue (don't use middle one)
         const increment = 3;
-        if (dPercentage < baseline - 4 * increment) {
+        if (dPercentage < baselineDPercentage - 4 * increment) {
             return _colors[0];
         }
-        if (dPercentage < baseline - 3 * increment) {
+        if (dPercentage < baselineDPercentage - 3 * increment) {
             return _colors[1];
         }
-        if (dPercentage < baseline - 2 * increment) {
+        if (dPercentage < baselineDPercentage - 2 * increment) {
             return _colors[2];
         }
-        if (dPercentage < baseline - 1 * increment) {
+        if (dPercentage < baselineDPercentage - 1 * increment) {
             return _colors[3];
         }
-        if (dPercentage < baseline) {
+        if (dPercentage < baselineDPercentage) {
             return _colors[4];
         }
 
-        if (dPercentage > baseline + 4 * increment) {
+        if (dPercentage > baselineDPercentage + 4 * increment) {
             return _colors[10];
         }
-        if (dPercentage > baseline + 3 * increment) {
+        if (dPercentage > baselineDPercentage + 3 * increment) {
             return _colors[9];
         }
-        if (dPercentage > baseline + 2 * increment) {
+        if (dPercentage > baselineDPercentage + 2 * increment) {
             return _colors[8];
         }
-        if (dPercentage > baseline + increment) {
+        if (dPercentage > baselineDPercentage + increment) {
             return _colors[7];
         }
         return _colors[6];
+    }
+
+    textFromDPercentage(dPercentage: number, baselineDPercentage = 50) {
+        if (dPercentage > baselineDPercentage) {
+            return "D +" + (dPercentage - baselineDPercentage).toFixed(1) + "%";
+        }
+        if (dPercentage < baselineDPercentage) {
+            return "R +" + (baselineDPercentage - dPercentage).toFixed(1) + "%";
+        }
+        return "Even";
     }
 
     onSliderChange = (value) => {
@@ -82,14 +95,23 @@ class App extends Component<{}, AppState> {
       }
 
     let stateColors = new Map<string, string>();
+    let nationalDPercentage = 50;
     if (this.state.stateNames && this.state.year) {
+        let electionData = this.state.electionData[this.state.year];
+        let dTotal = 0, rTotal = 0;
+        electionData.forEach(value => {
+            dTotal += value.dCount;
+            rTotal += value.rCount;
+        });
+        nationalDPercentage = (dTotal * 100.0) / (dTotal + rTotal);
+        let baselineDPercentage = (this.state.rawResults) ? 50 : nationalDPercentage;
         for (let i in this.state.stateNames) {
             let stateCode = this.state.stateNames[i].code;
             //TODO optimize
-            let stateData = _.find(this.state.electionData[this.state.year], electionDataObj => electionDataObj.stateCode === stateCode);
+            let stateData = _.find(electionData, electionDataObj => electionDataObj.stateCode === stateCode);
             if (stateData) {
                 // TODO - cooler stuff
-                stateColors[stateCode] = this.colorFromDAndRVote(stateData.dCount, stateData.rCount);
+                stateColors[stateCode] = this.colorFromDAndRVote(stateData.dCount, stateData.rCount, baselineDPercentage);
             }
         }
     }
@@ -105,8 +127,14 @@ class App extends Component<{}, AppState> {
                           y={0}
                           width={500}
                           height={500}/>
-            </svg>
-            <div>Year {this.state.year}</div>
+              </svg>
+              <div>
+                  <Button.Group>
+                      <Button active={this.state.rawResults} onClick={() => this.setState({ rawResults: true })}>Actual results</Button>
+                      <Button active={!this.state.rawResults} onClick={() => this.setState({ rawResults: false })}>Relative to popular vote</Button>
+                  </Button.Group>
+              </div>
+              <div>Year {this.state.year} Popular vote: {this.textFromDPercentage(nationalDPercentage, 100 - nationalDPercentage)}</div>
             <div style={{width: 500}}>
                 <Slider min={MIN_YEAR} max={MAX_YEAR} step={YEAR_STEP} value={this.state.year} onChange={this.onSliderChange}/>
             </div>
