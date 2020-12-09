a17f0ab0ca0f5252415dde8149baa56ead997dd9
gregstoll <greg@gregstoll.com>
2020-12-08 21:44:25 -0600

    restore test

--- stateElectionMap/src/App.test.tsx
+++ stateElectionMap/src/App.test.tsx
@@ -4,9 +4,8 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  //TODO
-  /*ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);*/
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
 
